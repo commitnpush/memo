9776602023b813001fe63d644736803cde141a9a
flynn <lognamu@gmail.com>
2019-02-27 18:17:54 +0900

    step01 - express api서버 구현 1.회원가입 기능추가 2.로그인/로그아웃 기능 추가 3.메모 CRUD 기능 추가

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,36 @@
-const express = require('express');
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import session from 'express-session';
+import api from './routes';
+
 const app = express();
-app.use(express.static('dist'));
+
+/* mongodb connection */
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => console.log('Connected to mongodb server'));
+mongoose.connect('mongodb://localhost/memo');
+
+/*use session */
+app.use(session({
+  secret: "Memo$1$234",
+  resave: false,
+  saveUninitialized:true
+}));
+
+
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+
+app.use('/api', api);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Server Error");
+});
 
 app.listen(8080, () => console.log("Listening on port 8080!"));
