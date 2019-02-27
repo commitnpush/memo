80782ac09ba6b8cab66a8147b3f4b4623b0b9d30
flynn <lognamu@gmail.com>
2019-02-27 16:15:24 +0900

    step00 - 프로젝트 기본환경구성

---
+++ src/client/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <h1>Project Start</h1>
+      </div>
+    );
+  }
+}
+export default App;
